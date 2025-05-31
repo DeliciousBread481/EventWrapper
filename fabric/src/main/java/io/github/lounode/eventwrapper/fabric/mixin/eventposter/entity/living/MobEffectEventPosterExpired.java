a93c8aff315908706c11a2a43e6a7dd59534922d
Lounode <lounode@gmail.com>
2025-06-01 00:15:02 +0800

    fix mobeffectevent onExpired do not call on expired bug

--- fabric/src/main/java/io/github/lounode/eventwrapper/fabric/mixin/eventposter/entity/living/MobEffectEventPosterExpired.java
+++ fabric/src/main/java/io/github/lounode/eventwrapper/fabric/mixin/eventposter/entity/living/MobEffectEventPosterExpired.java
@@ -1,24 +1,29 @@
 package io.github.lounode.eventwrapper.fabric.mixin.eventposter.entity.living;
 
+import com.llamalad7.mixinextras.sugar.Local;
+
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 import io.github.lounode.eventwrapper.EventsWrapper;
 import io.github.lounode.eventwrapper.event.entity.living.MobEffectEventWrapper;
 
-@Mixin(MobEffectInstance.class)
+@Mixin(LivingEntity.class)
 public class MobEffectEventPosterExpired {
 	@Inject(
-		method = "tick",
-		at = @At("TAIL")
+		method = "tickEffects",
+		at = @At(
+			value = "INVOKE",
+			target = "Ljava/util/Iterator;remove()V"
+		)
 	)
-	private void onTick(LivingEntity entity, Runnable onExpirationRunnable, CallbackInfoReturnable<Boolean> cir) {
-		EventsWrapper.post(new MobEffectEventWrapper.Expired(entity, (MobEffectInstance) (Object) this));
+	private void onExpired(CallbackInfo ci, @Local MobEffectInstance mobEffect) {
+		EventsWrapper.post(new MobEffectEventWrapper.Expired((LivingEntity) (Object) this, mobEffect));
 	}
 }
