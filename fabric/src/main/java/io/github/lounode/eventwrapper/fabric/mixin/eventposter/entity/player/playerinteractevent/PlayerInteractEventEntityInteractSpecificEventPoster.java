e802fb53cb20527e560e0975b5ef8e7f86b7ce66
Lounode <lounode@gmail.com>
2025-06-12 03:06:02 +0800

    fix #1

--- fabric/src/main/java/io/github/lounode/eventwrapper/fabric/mixin/eventposter/entity/player/playerinteractevent/PlayerInteractEventEntityInteractSpecificEventPoster.java
+++ fabric/src/main/java/io/github/lounode/eventwrapper/fabric/mixin/eventposter/entity/player/playerinteractevent/PlayerInteractEventEntityInteractSpecificEventPoster.java
@@ -1,10 +1,14 @@
 package io.github.lounode.eventwrapper.fabric.mixin.eventposter.entity.player.playerinteractevent;
 
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,10 +19,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import io.github.lounode.eventwrapper.fabric.EventWrapperHooks;
 
 @Mixin(targets = "net.minecraft.server.network.ServerGamePacketListenerImpl$1")
-public abstract class PlayerInteractEventEntityInteractSpecificEventPoster {
+public abstract class PlayerInteractEventEntityInteractSpecificEventPoster implements ServerboundInteractPacket.Handler {
 
 	@Shadow
-	protected abstract void performInteraction(InteractionHand hand, ServerGamePacketListenerImpl.EntityInteraction interaction);
+	@Final
+	private ServerGamePacketListenerImpl field_28963;
+
+	@Shadow
+	@Final
+	private Entity val$target;
 
 	@Inject(
 		method = "onInteraction(Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/Vec3;)V",
@@ -26,13 +35,12 @@ public abstract class PlayerInteractEventEntityInteractSpecificEventPoster {
 		cancellable = true
 	)
 	private void onInteract(InteractionHand hand, Vec3 interactionLocation, CallbackInfo ci) {
-		ci.cancel();
-		this.performInteraction(hand, ((serverPlayer, entity, interactionHand) -> {
-			InteractionResult result = EventWrapperHooks.onInteractEntityAt(serverPlayer, entity, interactionLocation, interactionHand);
-			if (result != null) {
-				return result;
-			}
-			return entity.interactAt(serverPlayer, interactionLocation, interactionHand);
-		}));
+		Player player = this.field_28963.getPlayer();
+
+		InteractionResult result = EventWrapperHooks.onInteractEntityAt(player, this.val$target, interactionLocation, hand);
+
+		if (result != null) {
+			ci.cancel();
+		}
 	}
 }
