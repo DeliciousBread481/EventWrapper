cda7e249c9fa4fb77290f5a48543554b27202d4a
DeliciousBread481 <3574467868@qq.com>
2026-05-18 20:26:15 +0400

    Fix crash because use client class on server with fakeplayer

--- common/src/main/java/io/github/lounode/eventwrapper/mixin/ItemCooldownEventPoster.java
+++ common/src/main/java/io/github/lounode/eventwrapper/mixin/ItemCooldownEventPoster.java
@@ -2,7 +2,6 @@ package io.github.lounode.eventwrapper.mixin;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemCooldowns;
 import net.minecraft.world.item.ServerItemCooldowns;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,14 +24,9 @@ public class ItemCooldownEventPoster {
 		at = @At("HEAD")
 	)
 	private void onCooldownRemove(Item item, CallbackInfo ci) {
-		ItemCooldowns self = (ItemCooldowns) (Object) this;
-		Player player;
-		if (self instanceof ServerItemCooldowns cooldowns) {
-			player = cooldowns.player;
-		} else {
-			player = ClientUtil.getClientPlayer();
-		}
-
+		ServerItemCooldowns self = (ServerItemCooldowns) (Object) this;
+		Player player = self.player;
+		
 		ItemCooldownFinishEventWrapper event = new ItemCooldownFinishEventWrapper(player, item);
 		EventsWrapper.post(event);
 	}
@@ -44,13 +38,8 @@ public class ItemCooldownEventPoster {
 		argsOnly = true
 	)
 	private int onCooldownStart(int ticks, Item item) {
-		ItemCooldowns self = (ItemCooldowns) (Object) this;
-		Player player;
-		if (self instanceof ServerItemCooldowns cooldowns) {
-			player = cooldowns.player;
-		} else {
-			player = ClientUtil.getClientPlayer();
-		}
+		ServerItemCooldowns self = (ServerItemCooldowns) (Object) this;
+		Player player = self.player;
 		ItemCooldownStartEventWrapper event = new ItemCooldownStartEventWrapper(player, item, ticks);
 		EventsWrapper.post(event);
 		return event.isCanceled() ? 0 : event.getTicks();
---
+++ fabric/src/main/java/io/github/lounode/eventwrapper/fabric/mixin/ItemCooldownEventPosterClient.java
@@ -0,0 +1,61 @@
+package io.github.lounode.eventwrapper.fabric.mixin;  
+  
+import net.fabricmc.api.EnvType;  
+import net.fabricmc.api.Environment;  
+import net.minecraft.world.entity.player.Player;  
+import net.minecraft.world.item.Item;  
+import net.minecraft.world.item.ItemCooldowns;  
+import net.minecraft.world.item.ServerItemCooldowns;  
+  
+import org.spongepowered.asm.mixin.Mixin;  
+import org.spongepowered.asm.mixin.injection.At;  
+import org.spongepowered.asm.mixin.injection.Inject;  
+import org.spongepowered.asm.mixin.injection.ModifyVariable;  
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;  
+  
+import io.github.lounode.eventwrapper.EventsWrapper;  
+import io.github.lounode.eventwrapper.event.entity.player.ItemCooldownFinishEventWrapper;  
+import io.github.lounode.eventwrapper.event.entity.player.ItemCooldownStartEventWrapper;  
+import io.github.lounode.eventwrapper.utils.ClientUtil;  
+  
+@Environment(EnvType.CLIENT)  
+@Mixin(ItemCooldowns.class)  
+public class ItemCooldownEventPosterClient {  
+  
+	@Inject(  
+		method = "removeCooldown",  
+		at = @At("HEAD")  
+	)  
+	private void onCooldownRemove(Item item, CallbackInfo ci) {  
+		ItemCooldowns self = (ItemCooldowns) (Object) this;  
+		Player player;  
+		if (self instanceof ServerItemCooldowns cooldowns) {  
+			player = cooldowns.player;  
+		} else {  
+			player = ClientUtil.getClientPlayer();  
+		}  
+  
+		ItemCooldownFinishEventWrapper event = new ItemCooldownFinishEventWrapper(player, item);  
+		EventsWrapper.post(event);  
+	}  
+  
+	@ModifyVariable(  
+		method = "addCooldown",  
+		at = @At("HEAD"),  
+		ordinal = 0,  
+		argsOnly = true  
+	)  
+	private int onCooldownStart(int ticks, Item item) {  
+		ItemCooldowns self = (ItemCooldowns) (Object) this;  
+		Player player;  
+		if (self instanceof ServerItemCooldowns cooldowns) {  
+			player = cooldowns.player;  
+		} else {  
+			player = ClientUtil.getClientPlayer();  
+		}  
+		ItemCooldownStartEventWrapper event = new ItemCooldownStartEventWrapper(player, item, ticks);  
+		EventsWrapper.post(event);  
+		return event.isCanceled() ? 0 : event.getTicks();  
+	}  
+  
+}
